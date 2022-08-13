433a27a695b43b9ada33193011e0ecbeab9d2ae2
gurgenkarapetyan <gurgen.karapetyan.noor@gmail.com>
2022-08-13 12:03:55 +0400

    - added project files

--- Source/ActionRoguelike/Public/SCharacter.h
+++ Source/ActionRoguelike/Public/SCharacter.h
@@ -8,67 +8,24 @@
 
 class UCameraComponent;
 class USpringArmComponent;
-class USInteractionComponent;
-class UAnimMontage;
-class USAttributeComponent;
-class UParticleSystem;
-class USActionComponent;
 
 UCLASS()
 class ACTIONROGUELIKE_API ASCharacter : public ACharacter
 {
 	GENERATED_BODY()
-
-protected:
-
-	/* VisibleAnywhere = read-only, still useful to view in-editor and enforce a convention. */
-	UPROPERTY(VisibleAnywhere, Category = "Effects")
-	FName TimeToHitParamName;
-
-	UPROPERTY(VisibleAnywhere)
-	USpringArmComponent* SpringArmComp;
-
-	UPROPERTY(VisibleAnywhere)
-	UCameraComponent* CameraComp;
-
-	UPROPERTY(VisibleAnywhere)
-	USInteractionComponent* InteractionComp;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
-	USAttributeComponent* AttributeComp;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
-	USActionComponent* ActionComp;
-
-	void MoveForward(float Value);
-
-	void MoveRight(float Value);
-
-	void SprintStart();
-
-	void SprintStop();
-
-	void PrimaryAttack();
-
-	void BlackHoleAttack();
-
-	void Dash();
-
-	void PrimaryInteract();
-
-	UFUNCTION()
-	void OnHealthChanged(AActor* InstigatorActor, USAttributeComponent* OwningComp, float NewHealth, float Delta);
-
-	virtual void PostInitializeComponents() override;
-
-	virtual FVector GetPawnViewLocation() const override;
-
+	
 public:	
-
 	ASCharacter();
 
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-	UFUNCTION(Exec)
-	void HealSelf(float Amount = 100);
+protected:
+
+	UPROPERTY(VisibleAnywhere)
+	USpringArmComponent* SpringArmComponent;
+
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* CameraComponent;
+
+	void MoveForward(float Value);
 };
